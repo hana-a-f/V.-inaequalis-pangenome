7077b602b7f8387e92c96651a53dff57325ae8c2
Hana Feulner <hf332@cornell.edu>
2026-09-19 20:47:45 -0400

    Create run_all_mitohifi.sh

---
+++ genome_assembly/run_all_mitohifi.sh
@@ -0,0 +1,223 @@
+mkdir -p VI_1797_2 && cd VI_1797_2 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_1797_2_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
+
+mkdir -p VI_1797_9 && cd VI_1797_9 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_1797_9_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
+
+mkdir -p VI_18_019 && cd VI_18_019 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_18_019_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
+
+mkdir -p VI_18_030 && cd VI_18_030 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_18_030_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
+
+mkdir -p VI_18_033 && cd VI_18_033 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_18_033_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
+
+mkdir -p VI_18_037 && cd VI_18_037 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_18_037_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
+
+mkdir -p VI_18_043 && cd VI_18_043 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_18_043_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
+
+mkdir -p VI_19_004 && cd VI_19_004 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_19_004_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
+
+mkdir -p VI_19_011 && cd VI_19_011 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_19_011_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
+
+mkdir -p VI_EU104 && cd VI_EU104 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_EU104_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
+
+mkdir -p VI_EU160 && cd VI_EU160 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_EU160_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
+
+mkdir -p VI_EU301 && cd VI_EU301 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_EU301_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
+
+mkdir -p VI_EU302 && cd VI_EU302 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_EU302_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
+
+mkdir -p VI_EU413 && cd VI_EU413 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_EU413_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
+
+mkdir -p VI_EUNL19 && cd VI_EUNL19 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_EUNL19_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
+
+mkdir -p VI_EUNL24 && cd VI_EUNL24 && \
+singularity run \
+  --bind $PWD \
+  --bind /workdir/hf332/mito \
+  --bind /workdir/hf332/mito/mito_reference \
+  --pwd $PWD \
+  /programs/mitohifi-3.0.0/mitohifi.sif \
+  mitohifi.py \
+  -c ../VI_EUNL24_hifi.asm.bp.p_ctg.fa \
+  -f /workdir/hf332/mito/mito_reference/PV785816.1.fasta \
+  -g /workdir/hf332/mito/mito_reference/PV785816.1.gb \
+  -t 24 \
+  -a fungi; cd ..
